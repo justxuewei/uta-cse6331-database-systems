b5a665384224396aa94d9e4f414d55c91914dcef
Xuewei Niu <justxuewei@apache.org>
2022-06-17 18:30:59 +0800

    feat: assignment 2
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import config
 
 from datetime import datetime
 from flask import Flask, render_template, request, redirect
+from math import radians, cos, sin, asin, sqrt
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -18,6 +19,120 @@ db = pymysql.connect(
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+def get_range(range_str):
+    arr = range_str.split('-')
+    if len(arr) != 2:
+        raise ValueError
+    try:
+        return float(arr[0]), float(arr[1])
+    except Exception as e:
+        raise e
+
+
+def select_all(sql):
+    cursor = db.cursor()
+    print(sql)
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    field_names = [i[0] for i in cursor.description]
+    data = []
+    for row in results:
+        row_data = {}
+        for i, field_name in enumerate(field_names):
+            row_data[field_name] = row[i]
+        data.append(row_data)
+    return data
+
+
+def select_one(sql):
+    cursor = db.cursor()
+    print(sql)
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    data = {}
+    field_names = [i[0] for i in cursor.description]
+    for i, field_name in enumerate(field_names):
+        data[field_name] = result[i]
+    return data
+
+
+def message_page(msg):
+    return render_template("message.html", msg=msg)
+
+
+def get_distance(lng1, lat1, lng2, lat2):
+    lng1, lat1, lng2, lat2 = map(
+        radians, [float(lng1), float(lat1), float(lng2), float(lat2)])
+    dlon = lng2-lng1
+    dlat = lat2-lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    distance = 2*asin(sqrt(a))*6371*1000  # 地球平均半径，6371km
+    distance = round(distance/1000, 3)
+    return distance
+
+
+@app.route("/mag_greater_than_5")
+def mag_greater_than_5():
+    sql = "SELECT * FROM earthquakes WHERE mag >= 5"
+    data = select_all(sql)
+    sql = "SELECT COUNT(*) as cnt FROM earthquakes WHERE mag >= 5"
+    cnt = select_one(sql)['cnt']
+    return render_template("result.html", data=data, cnt=cnt)
+
+
+@app.route("/queries")
+def queries():
+    return render_template("queries.html")
+
+
+@app.route("/mags")
+def mags():
+    started_date = request.args.get("started_date")
+    ended_date = request.args.get("ended_date")
+    mag_range = request.args.get("mag_range")
+    if started_date is None:
+        return render_template("message.html", msg="started_date is required")
+    if ended_date is None:
+        return render_template("message.html", msg="ended_date is required")
+    if mag_range is None:
+        return render_template("message.html", msg="mag_range is required")
+
+    try:
+        low, high = get_range(mag_range)
+    except Exception as e:
+        return message_page("failed to retrieve mag range, err = {}".format(e))
+
+    started = datetime.strptime(started_date, "%Y-%m-%d")
+    ended = datetime.strptime(ended_date, "%Y-%m-%d")
+    sql = "SELECT * FROM earthquakes WHERE mag >= {} AND mag <= {} AND time >= '{}' AND time <= '{}'".format(
+        low, high, started, ended
+    )
+    data = select_all(sql)
+    return render_template("result.html", data=data)
+
+
+@app.route("/near")
+def near():
+    latitude = request.args.get("latitude")
+    longitude = request.args.get("longitude")
+    distance = request.args.get("distance")
+
+    try:
+        latitude = float(latitude)
+        longitude = float(longitude)
+        distance = float(distance)
+    except Exception as e:
+        return message_page("latitude, longitude and distance should be float values, err = {}".format(e))
+
+    sql = "SELECT * FROM earthquakes"
+    all_data = select_all(sql)
+    data = []
+    for earthquake in all_data:
+        if get_distance(longitude, latitude, earthquake['longitude'], earthquake['latitude']) <= distance:
+            data.append(earthquake)
+    return render_template("result.html", data=data)
 
 
 @app.route('/bounding_box')
@@ -36,10 +151,10 @@ def bounding_box():
         x2 = float(x2)
         y2 = float(y2)
     except Exception as e:
-        return render_template("error.html", msg="value type is unsupported, err = {}".format(e))
+        return render_template("message.html", msg="value type is unsupported, err = {}".format(e))
 
     if x1 > x2 or y1 > y2:
-        return render_template("error.html", msg="x1, y1 should less than x2, y2, respectively")
+        return render_template("message.html", msg="x1, y1 should less than x2, y2, respectively")
 
     cursor = db.cursor()
     cursor.execute(
@@ -90,7 +205,7 @@ def single_date():
     time_range = request.args.get("time_range")
     if date is None or time_range is None:
         return render_template("single_date.html")
-    
+
     low, high = get_range(time_range)
     low = int(low)
     high = int(high)
@@ -108,15 +223,8 @@ def single_date():
     cursor.execute(sql)
     result = cursor.fetchone()
     print(result)
-    
+
     return render_template("single_date_result.html", count=result[0], net=result[1])
-
-
-def get_range(range_str):
-    arr = range_str.split('-')
-    if len(arr) != 2:
-        return 0, 0
-    return float(arr[0]), float(arr[1])
 
 
 @app.route("/replace_nn")
@@ -125,14 +233,15 @@ def replace_nn():
     rnn = request.args.get("rnn")
     if nn is None or rnn is None:
         return render_template("replace_nn.html")
-    
+
     cursor = db.cursor()
     sql = "UPDATE earthquakes SET net = '{}' WHERE net = '{}'".format(rnn, nn)
     print(sql)
     rows = cursor.execute(sql)
     db.commit()
 
-    return render_template("error.html", msg="successfully update, affected rows = {}".format(rows))
+    return render_template("message.html", msg="successfully update, affected rows = {}".format(rows))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
--- migration.py
+++ migration.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 
 engine = create_engine('mysql://root:12345678@127.0.0.1:3306/cloudcomputing')
 
-df = pd.read_csv("./static/ds2.csv", sep=',', encoding='utf8')
+df = pd.read_csv("all_month.csv", sep=',', encoding='utf8')
 df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%dT%H:%M:%S.%fZ')
+df['updated'] = pd.to_datetime(df['updated'], format='%Y-%m-%dT%H:%M:%S.%fZ')
 df.to_sql('earthquakes', con=engine, index=False, if_exists='append')
