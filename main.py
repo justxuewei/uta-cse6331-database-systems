35611d0a457d18ed2466e0d41ca6c5b2c45d0171
Xuewei Niu <justxuewei@apache.org>
2022-07-06 14:38:34 +0800

    feat: quiz 5 part 1
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- main.py
+++ main.py
@@ -1,97 +1,24 @@
-from cmath import asin, cos, sin, sqrt
-from datetime import datetime
-from dateutil import parser
-from dis import dis
-import json
-from math import radians
-import random
-import time
-import redis
-import pymysql
-import pickle
+from datetime import datetime, timedelta
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-db = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="12345678",
-    database="cloudcomputing"
-)
+###### Variables ######
 
-redis_ins = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-
-class MyEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, datetime):
-            return o.isoformat()
-
-        return json.JSONEncoder.default(self, o)
+# game1 status: "Closed", "Waiting", "Started"
+game1_status = "Closed"
+game1_closed_time = datetime.now()
+game1_player1 = None
+game1_player2 = None
+game1_current_player = None
+game1_min = None
+game1_max = None
+game1_remaining = None
 
 
-def get_distance(lng1, lat1, lng2, lat2):
-    lng1, lat1, lng2, lat2 = map(
-        radians, [float(lng1), float(lat1), float(lng2), float(lat2)])
-    dlon = lng2-lng1
-    dlat = lat2-lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    distance = 2*asin(sqrt(a))*6371*1000
-    distance = round(distance/1000, 3)
-    return distance
-
-
-def cache_key(task_name, *params):
-    param_strs = []
-    for param in param_strs:
-        param_strs.append(str(param))
-    params_key = "-".join(param_strs)
-    return "{}-{}".format(task_name, params_key)
-
-
-def get_range(range_str):
-    arr = range_str.split('-')
-    print(arr)
-    if len(arr) != 2:
-        raise ValueError(
-            "Invalid range format, for example, the valid format is `10-200`.")
-    try:
-        low, high = float(arr[0]), float(arr[1])
-    except Exception as e:
-        raise e
-    if low > high:
-        raise ValueError("low is greater than high")
-    return low, high
-
-
-def select_all(sql):
-    cursor = db.cursor()
-    print(sql)
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    field_names = [i[0] for i in cursor.description]
-    data = []
-    for row in results:
-        row_data = {}
-        for i, field_name in enumerate(field_names):
-            row_data[field_name] = row[i]
-        data.append(row_data)
-    return data
-
-
-def select_one(sql):
-    cursor = db.cursor()
-    print(sql)
-    cursor.execute(sql)
-    result = cursor.fetchone()
-    data = {}
-    field_names = [i[0] for i in cursor.description]
-    for i, field_name in enumerate(field_names):
-        data[field_name] = result[i]
-    return data
+###### Utils ######
 
 
 def message_page(msg):
@@ -112,60 +39,248 @@ def queries():
     return render_template("queries.html")
 
 
-@app.route("/task1")
-def task1():
-    n = request.args.get("n")
+###### Pages ######
+
+
+@app.route("/game1/admin")
+def page_game1_admin():
+    return render_template("game1_admin.html")
+
+
+@app.route("/game1/player1")
+def page_game1_player1():
+    return render_template("game1_player1.html")
+
+
+@app.route("/game1/player2")
+def page_game1_player2():
+    return render_template("game1_player2.html")
+
+###### APIs ######
+
+
+# role: public
+@app.route("/game1/status")
+def api_game1_status():
+    global game1_status
+    global game1_closed_time
+    global game1_min, game1_max, game1_remaining
+    remaining_time = datetime.timestamp(
+        game1_closed_time) - datetime.timestamp(datetime.now())
+    if remaining_time <= 0:
+        if game1_status == "Started":
+            game1_status = "Closed"
+        remaining_time = 0
+    if game1_status != "Started":
+        remaining_time = 0
+    return jsonify({
+        "status": game1_status,
+        "remaining_time": remaining_time,
+        "min": game1_min,
+        "max": game1_max,
+        "remaining": game1_remaining,
+    })
+
+
+# role: public
+@app.route("/game1/players")
+def api_game1_players():
+    global game1_player1, game1_player2
+    return jsonify({
+        "player1": game1_player1,
+        "player2": game1_player2,
+    })
+
+
+# role: public
+@app.route("/game1/current_player")
+def api_game1_current_player():
+    global game1_status, game1_current_player, game1_player1, game1_player2
+    ret = None
+    if game1_status != "Closed":
+        ret = game1_current_player
+    if ret == "player1":
+        ret = game1_player1
+    elif ret == "player2":
+        ret = game1_player2
+    return jsonify({
+        "current_player": ret
+    })
+
+# role: admin
+
+
+@app.route("/game1/prestarted")
+def api_game1_prestarted():
+    role = request.args.get("role")
+    if not role or role != "admin":
+        return jsonify({
+            "msg": "you don't have permission to prestart the game",
+        }), 500
+    global game1_player1, game1_player2, game1_status, game1_current_player, game1_min, game1_max, game1_remaining
+    game1_player1 = None
+    game1_player2 = None
+    game1_current_player = None
+    game1_min = None
+    game1_max = None
+    game1_remaining = None
+    game1_status = "Waiting"
+    return jsonify({
+        "msg": "success",
+    })
+
+
+# role: admin
+@app.route("/game1/start")
+def api_game1_start():
+    role = request.args.get("role")
+    if not role or role != "admin":
+        return jsonify({
+            "msg": "you don't have permission to prestart the game",
+        }), 500
     try:
-        n = int(n)
+        min_value = int(request.args.get("min"))
+        max_value = int(request.args.get("max"))
+        start_value = int(request.args.get("start"))
+        duration_value = int(request.args.get("duration"))
+        player_value = request.args.get("player")
     except Exception as e:
-        return message_page("n should be a number")
-    names = request.args.get("names")
-    if not n or not names:
-        return message_page("n and names are required")
-    print(n, names)
-    names_arr = names.split(",")
-    print(names_arr)
-    if len(names_arr) != n:
-        return message_page("the length of names is not equals to n")
-    data = []
-    for name in names_arr:
-        sql = "select count(*) as cnt from quiz4 where col_4 = '{}'".format(name)
-        sql_data = select_one(sql)
-        data.append({
-            'fruit': name,
-            'cnt': sql_data['cnt']
+        return jsonify({
+            "msg": "invalid parameters, err = {}".format(e),
+        }), 500
+    print("min_value = {}, max_value = {}, start_value = {}, duration_value = {}".format(
+        min_value, max_value, start_value, duration_value))
+    global game1_player1, game1_player2, game1_status, game1_closed_time, game1_current_player, game1_min, game1_max, game1_remaining
+    if not game1_player1 or not game1_player2:
+        return jsonify({
+            "msg": "players are not ready",
+        }), 500
+    if game1_status != "Waiting":
+        return jsonify({
+            "msg": "game can't be started due to current status",
+        }), 500
+    game1_status = "Started"
+    game1_closed_time = datetime.now() + timedelta(seconds=duration_value)
+    if not player_value:
+        return jsonify({
+            'msg': 'the player is required'
+        }), 500
+    if player_value != "player1" and player_value != "player2":
+        return jsonify({
+            'msg': 'the player is invalid, the possible values are "player1" or "player2"'
+        }), 500
+    game1_current_player = player_value
+    game1_min = min_value
+    game1_max = max_value
+    game1_remaining = start_value
+    return jsonify({
+        "msg": "success",
+    })
+
+
+# role: admin
+@app.route("/game1/close")
+def api_game1_close():
+    role = request.args.get("role")
+    if not role or role != "admin":
+        return jsonify({
+            "msg": "you don't have permission to prestart the game",
+        }), 500
+    global game1_player1, game1_player2, game1_status, game1_current_player, game1_min, game1_max, game1_remaining
+    game1_player1 = None
+    game1_player2 = None
+    game1_current_player = None
+    game1_min = None
+    game1_max = None
+    game1_remaining = None
+    game1_status = "Closed"
+    return jsonify({
+        "msg": "success",
+    })
+
+
+# role: player
+@app.route("/game1/join")
+def api_game1_join():
+    role = request.args.get("role")
+    if not role or (role != "player1" and role != "player2"):
+        return jsonify({
+            "msg": "you don't have permission to join a game",
+        }), 500
+    name = request.args.get("name")
+    if not name:
+        return jsonify({
+            "msg": "name is required"
+        }), 500
+    global game1_status, game1_player1, game1_player2
+    if game1_status != "Waiting":
+        return jsonify({
+            "msg": "game is not in waiting status"
+        }), 500
+    if role == "player1":
+        game1_player1 = name
+    if role == "player2":
+        game1_player2 = name
+    return jsonify({
+        "msg": "joined successfully"
+    })
+
+
+@app.route("/game1/player_submit")
+def api_game1_player_submit():
+    global game1_player1, game1_player2, game1_status, game1_current_player, game1_min, game1_max, game1_remaining
+    role = request.args.get("role")
+    if not role or (role != "player1" and role != "player2"):
+        return jsonify({
+            "msg": "you don't have permission to join a game",
+        }), 500
+    if role != game1_current_player:
+        return jsonify({
+            "msg": "it's not your turn"
+        }), 500
+    value = request.args.get("value")
+    try:
+        value = int(value)
+    except:
+        return jsonify({
+            'msg': 'the value should be a integer'
+        }), 500
+
+    if value < game1_min or value > game1_max:
+        return jsonify({
+            'msg': 'the value should be between {} and {}'.format(game1_min, game1_max)
+        }), 500
+    result = game1_remaining - value
+    if result < 0:
+        return jsonify({
+            'msg': 'the value should be less than {}'.format(game1_remaining)
+        }), 500
+    elif result == 0:
+        if game1_current_player == 'player1':
+            player_name = game1_player1
+        else:
+            player_name = game1_player2
+        game1_player1 = None
+        game1_player2 = None
+        game1_current_player = None
+        game1_min = None
+        game1_max = None
+        game1_remaining = None
+        game1_status = "Closed"
+        return jsonify({
+            'msg': '{} loss the game'.format(player_name),
+            'remaining': result,
         })
-    print(data)
-    return results_page(data, None, task_name='task_1')
-
-
-@app.route("/task2")
-def task2():
-    n = request.args.get("n")
-    try:
-        n = int(n)
-    except Exception as e:
-        return message_page("n should be a number")
-    if not n:
-        return message_page("n and names are required")
-    sql = "select count(0) as cnt, col_4 as fruit from quiz4 group by col_4 order by cnt desc limit {}".format(n)
-    data = select_all(sql)  
-    print(data)
-    return results_page(data, None, task_name='task_2')
-
-
-@app.route("/task3")
-def task3():
-    range = request.args.get("range")
-    try:
-        low, high = get_range(range)
-        low, high = int(low), int(high)
-    except Exception as e:
-        return message_page("Failed to parse range, err = {}".format(e))
-    sql = "select * from quiz4 where col_1 >= {} and col_1 <= {}".format(low, high)
-    data = select_all(sql)
-    print(data)
-    return results_page(data, None, task_name='task_3')
+    else:
+        if game1_current_player == 'player1':
+            game1_current_player = 'player2'
+        else:
+            game1_current_player = 'player1'
+        game1_remaining = result
+        return jsonify({
+            'remaining': result,
+            'msg': 'continue',
+        })
 
 
 if __name__ == "__main__":
